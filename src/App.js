1f122407d4143be16f4877540d9e22eb56bff9f6
MacDarki <clem0123@hotmail.fr>
2022-01-26 12:43:48 +0100

    v1.2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Component/Header';
 
 function App() {
+  return (
+    <Header />
+  );
+
+}
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -15,11 +22,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn YU-GI-OH !!!
         </a>
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
---
+++ src/Component/Header.js
@@ -0,0 +1,21 @@
+function Header() {
+    return (
+        <div className="App">
+        <header className="App-header">
+        
+          <p>
+            <h1>Bienvenue sur le site test REACT.</h1>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn YU-GI-OH !!!
+          </a>
+        </header>
+      </div>
+    )
+}
+export default Header
